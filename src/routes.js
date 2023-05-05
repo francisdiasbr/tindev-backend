706f4b5ccab296ad54012b4169438b44d069608e
Francis Dias <francisdiasbr@gmail.com>
2023-05-05 16:09:23 -0300

    cria o controller do Dev

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,13 @@ const express = require('express')
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  
   return res.json({ message: `Hello ${req.query.name}` })
+});
+
+routes.post('/devs', (req, res) => {
+  console.log(req.body);
+
+  return res.json({ ok: true })
 });
 
 module.exports = routes;
